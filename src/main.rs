fe4dbe62de85c0c308ecb419bbac1b0e78671f8d
xermic <bigcyrill@hotmail.com>
2017-10-24 21:48:20 +0200

    first playable version

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use pancurses::Input::*;
 
 const DIM_X: usize = 42;
 const DIM_Y: usize = 22;
-const FPS: u64 = 4;
 const SNAKE_COLOR_ID: u16 = 1;
 const APPLE_COLOR_ID: u16 = 2;
 const EMPTY_COLOR_ID: u16 = 3;
@@ -22,56 +21,85 @@ const O_WALL: char = '#';
 
 
 struct State {
-    field: [[char; DIM_X]; DIM_Y],
-    snake: Vec<(i8, i8)>,
-    dir: (i8, i8),
-    vel: (i8, i8),
-    apple: Vec<(i8, i8)>,
+    snake: Vec<(i32, i32)>,
+    dir: (i32, i32),
+    vel: i32,
+    apple: Vec<(i32, i32)>,
+    wall: Vec<(i32, i32)>
 }
 
 
 fn init() -> State {
     let mut state =  State {
-        field: [[' '; DIM_X]; DIM_Y],
-        snake: vec![(rand::thread_rng().gen_range(1, DIM_Y as i8 -1), rand::thread_rng().gen_range(1, DIM_X as i8 -1))],
+        snake: vec![get_rand_tuple()],
         dir: (1,0),
-        vel: (1,1),
-        apple: vec![(rand::thread_rng().gen_range(1, DIM_Y as i8 -1), rand::thread_rng().gen_range(1, DIM_X as i8 -1))],
+        vel: 4,
+        apple: vec![get_rand_tuple()],
+        wall: Vec::new()
     };
 
     for x in 0..(DIM_Y) {
         for y in 0..(DIM_X) {
             if x==0 || x==DIM_Y-1 || y==0 || y==DIM_X-1 {
-                state.field[x][y] = O_WALL;
+                state.wall.push((x as i32, y as i32));
             }
         }
     }
-    state.field[state.snake[0].0 as usize][state.snake[0].1 as usize] = O_SNAKE;
-    state.field[state.apple[0].0 as usize][state.apple[0].1 as usize] = O_APPLE;
 
     state
 }
 
 
+fn get_rand_tuple() -> (i32, i32) {
+        (rand::thread_rng().gen_range(1, DIM_Y as i32 -1), rand::thread_rng().gen_range(1, DIM_X as i32 -1))
+}
+
+
 fn render(s: &State, w: &pancurses::Window) {
     w.clear();
+
+    w.attrset(pancurses::COLOR_PAIR(EMPTY_COLOR_ID as u32));
+    for e in &s.wall {
+        w.mvaddch(e.0, e.1, O_WALL);
+    }
+
+    w.attrset(pancurses::COLOR_PAIR(SNAKE_COLOR_ID as u32));
+    for e in &s.snake {
+        w.mvaddch(e.0, e.1, O_SNAKE);
+    }
+
+    w.attrset(pancurses::COLOR_PAIR(APPLE_COLOR_ID as u32));
+    for e in &s.apple {
+        w.mvaddch(e.0, e.1, O_APPLE);
+    }
+
+    w.attrset(pancurses::COLOR_PAIR(EMPTY_COLOR_ID as u32));
+    w.mvaddch(DIM_Y as i32, DIM_X as i32, '\n');
     w.addstr("Score: ");
     w.addstr(&s.snake.len().to_string());
-    w.addstr("\n\n");
-    for line in s.field.iter() {
-        for tile in line.iter() {
-            match tile {
-                &O_SNAKE => w.attrset(pancurses::COLOR_PAIR(SNAKE_COLOR_ID as u32)),
-                &O_APPLE => w.attrset(pancurses::COLOR_PAIR(APPLE_COLOR_ID as u32)),
-                _ => w.attrset(pancurses::COLOR_PAIR(EMPTY_COLOR_ID as u32))
-            };
-            w.addch(tile.to_owned());
-        }
-        w.addch('\n');
-    }
+
     w.refresh();
 }
 
+
+fn highscore(h: usize, w: &pancurses::Window) {
+    w.attrset(pancurses::COLOR_PAIR(EMPTY_COLOR_ID as u32));
+    let mut counter: usize = h;
+    while counter > 1 {
+        counter -= 1;
+        w.clear();
+        w.mvaddstr(10, 20 ,"Score: ");
+        w.addstr(&(h-counter).to_string());
+        w.refresh();
+        thread::sleep(time::Duration::from_millis(1000 / counter as u64));
+    }
+    w.attrset(pancurses::COLOR_PAIR(APPLE_COLOR_ID as u32));
+    w.clear();
+    w.mvaddstr(10, 20 ,"Score: ");
+    w.addstr(&h.to_string());
+    w.refresh();
+    thread::sleep(time::Duration::from_millis(3000));
+}
 
 
 fn input(s: &mut State, input: pancurses::Input) {
@@ -85,9 +113,9 @@ fn input(s: &mut State, input: pancurses::Input) {
 }
 
 
-fn update(mut s: &mut State, w: &pancurses::Window) {
+fn update(mut s: &mut State, w: &pancurses::Window) -> bool {
     // tick
-    let wait_fps = time::Duration::from_millis(1000 / FPS);
+    let wait_fps = time::Duration::from_millis(1000 / s.vel as u64);
     thread::sleep(wait_fps);
 
     // input
@@ -95,16 +123,43 @@ fn update(mut s: &mut State, w: &pancurses::Window) {
         input(&mut s, keypress);
     }
 
-    s.snake[0].0 += s.dir.0;
-    s.snake[0].1 += s.dir.1;
-    s.field[s.snake[0].0 as usize][s.snake[0].1 as usize] = O_SNAKE;
+    // move
+    let mut next: (i32, i32) = (s.snake[0].0 + s.dir.0, s.snake[0].1 + s.dir.1);
+    if s.wall.contains(&next) {
+        if next.1 >= DIM_X as i32 -1 {
+            next.1 = 1;
+        } else if next.1 <= 1 {
+            next.1 = DIM_X as i32 -2;
+        } else if next.0 >= DIM_Y as i32 -1 {
+            next.0 = 1;
+        } else if next.0 <= 1 {
+            next.0 = DIM_Y as i32 -2;
+        }
+    }
+
+    // check collision
+    if s.snake.contains(&next) {
+        return false
+    }
+
+    // check apple
+    if let Some(index) = s.apple.iter().position(|&r| r == next) {
+        s.apple.remove(index);   
+        s.apple.push(get_rand_tuple());
+        s.vel += 1;
+    } else {
+        s.snake.pop();
+    }
+
+    s.snake.insert(0, next);
+
+    true
 }
+
 
 fn main() {
     let window = pancurses::initscr();
     let result = std::panic::catch_unwind(|| {
-        let mut game_state = init();
-
         pancurses::start_color();
         pancurses::init_pair(SNAKE_COLOR_ID as i16, SNAKE_COLOR, BG_COLOR);
         pancurses::init_pair(APPLE_COLOR_ID as i16, APPLE_COLOR, BG_COLOR);
@@ -115,14 +170,17 @@ fn main() {
         window.nodelay(true);
         window.keypad(true);
     
-        loop {
-            update(&mut game_state, &window);
+        let mut game_state = init();
+
+        while update(&mut game_state, &window) {
             render(&game_state, &window);
         }
+        
+        highscore(game_state.snake.len(), &window);
     });
 
     pancurses::endwin();
-        if let Err(e) = result {
+    if let Err(e) = result {
         if let Some(e) = e.downcast_ref::<&'static str>() {
             writeln!(&mut std::io::stderr(), "Error: {}", e).unwrap();
         } else {
